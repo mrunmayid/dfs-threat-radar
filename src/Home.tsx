49ae3bc024fc55a4bbc446772410a747ab611c0e
Mrunmayi Davare <mrunmayi.dawre@gmail.com>
2024-10-24 21:58:47 -0500

    adding desc
    Adding desc to the home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Home: React.FC = () => {
   return (
     <div>
       <h1>DFS Threat Radar.</h1>
-        <p className="read-the-docs">
-          A single dashboard to track all you code vulnerabilities.
-        </p>
+      <p className="read-the-docs">
+        A single dashboard to track all you code vulnerabilities.
+      </p>
+
+      <p>
+        Gathering all types of vulnerabilities in one place is a fundamental
+        practice for any organization serious about cybersecurity. It enhances
+        visibility, improves efficiency, and ultimately strengthens the overall
+        security posture, safeguarding the organization against the ever-present
+        threat of cyber attacks.
+      </p>
     </div>
   );
 };
